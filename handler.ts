ee85185c6c02fd5219dbdcb179bf9bdfa9b52cf3
Callum McClean <callum.mcclean@dittosustainability.ai>
2021-03-10 10:51:09 +0000

    Green Test - Throws error on unexpected input

--- handler.ts
+++ handler.ts
@@ -1,8 +1,12 @@
 import { BatchOrderEvent, OrderBatched } from './lib/types'
 
 const handler = (event: BatchOrderEvent, context: any) => {
-  const batch = JSON.parse(event.body) as OrderBatched;
-  return batch;
+  try {
+    const batch = JSON.parse(event.body) as OrderBatched;
+    return batch;
+  } catch (error) {
+    throw new Error('Bad Request - Unable to process batch.');
+  }
 }
 
 module.exports = handler
