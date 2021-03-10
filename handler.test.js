bfdea8740847be67e097abf45a905750ecc4ccf9
Callum McClean <callum.mcclean@dittosustainability.ai>
2021-03-10 11:11:14 +0000

    Red test - split orders into catagories

--- handler.test.js
+++ handler.test.js
@@ -20,10 +20,23 @@ describe('Handler', () => {
   })
 
   it('Removes orders that should not be processed', () => {
-    const orders = handler(event, context)
+    const [fulfill, cancel] = handler(event, context)
 
+    const orders = [...fulfill, ...cancel]
     expect(orders.length).toEqual(2)
 
     expect(orders.some((order) => order.O_ID === "50022251")).toBe(false)
+  })
+
+  it('Split orders into fulfillments/cancellations', () => {
+    const [fulfill, cancel] = handler(event, context)
+
+    expect(fulfill.length).toEqual(1)
+    expect(cancel.length).toEqual(1)
+
+    const [fulfilOrder] = fulfill
+    const [cancelOrder] = cancel
+    expect(fulfilOrder.O_ID).toEqual('12345')
+    expect(cancelOrder.O_ID).toEqual('500324412')
   })
 })
