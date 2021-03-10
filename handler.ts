fc718c0b2c71c865dcc432c96b7cbe8c7124d032
Callum McClean <callum.mcclean@dittosustainability.ai>
2021-03-10 10:32:09 +0000

    Green test - parse event body to json

--- handler.ts
+++ handler.ts
@@ -1,7 +1,7 @@
 import { BatchOrderEvent } from './lib/types'
 
 const handler = (event: BatchOrderEvent, context: any) => {
-  return event.body
+  return JSON.parse(event.body)
 }
 
 module.exports = handler
