5c51bf9cbcdcf5ca91523ee8fee98b917823df66
Callum McClean <callum.mcclean@dittosustainability.ai>
2021-03-10 11:28:13 +0000

    Refactor categorise function to make use of lodash

--- lib/orderOperations/categoriseOrders.ts
+++ lib/orderOperations/categoriseOrders.ts
@@ -1,17 +1,12 @@
+import { partition } from 'lodash';
 import { LineItem, Order } from '../types';
 
-export const categoriseOrders = (orders: Order[]): [Order[], Order[]] => {
-  const fulfillOrders: Order[] = [];
-  const cancelOrders: Order[] = [];
+const isForFulfillment = (order: Order): Boolean => {
+  return order.ORDER_LINES.some((line: LineItem) => !!parseInt(line.QUANTITY))
+}
 
-  orders.forEach((order: Order) => {
-    if (order.ORDER_LINES.some((line: LineItem) => !!parseInt(line.QUANTITY))) {
-      fulfillOrders.push(order)
-    }
-    else {
-      cancelOrders.push(order)
-    }
-  })
-  
+export const categoriseOrders = (orders: Order[]): [Order[], Order[]] => {
+  // Referenced lodash documentation can be found here: https://lodash.com/docs/4.17.15#partition
+  const [fulfillOrders, cancelOrders] = partition(orders, isForFulfillment)
   return [fulfillOrders, cancelOrders];
 }
